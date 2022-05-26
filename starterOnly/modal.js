cb836f830acd98bee0e38b612733e75e0e0f4c99
Gavroche Woerly <gavrochewy@gmail.com>
2022-05-26 18:37:00 +0200

    Ajout de l'évènement pour la fermeture de la popup

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,61 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalCloseBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+modalCloseBtn.addEventListener('click', closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// User informations 
+const userInformations = {
+  first : '',
+  last : '',
+  email : '',
+  birthdate : '',
+  quantity : '',
+  location : '',
+  conditions : false,
+  newsletter : false,
+}
+
+// Modifiation du prénom
+document.getElementById('first').addEventListener('change', function(e) {
+  e.preventDefault;
+  userInformations.first = e.target.value;
+  console.log(userInformations)
+})
+
+// Modifiation du nom
+document.getElementById('last').addEventListener('change', function(e) {
+  e.preventDefault;
+  userInformations.last = e.target.value;
+  console.log(userInformations)
+})
+
+// Modifiation de l'email
+document.getElementById('email').addEventListener('change', function(e) {
+  e.preventDefault;
+  userInformations.email = e.target.value;
+  console.log(userInformations)
+})
+
+
+
+
+
+
+
 
 
